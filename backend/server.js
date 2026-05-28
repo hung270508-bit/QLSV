618ba40dea396ec70925c855c902643e40f3b15b
Nguyen The Vinh <vinhnguyen5859@gmail.com>
2026-05-28 10:32:08 +0700

    tạo refresh và sẻarch trong quanlykhoa

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '1234',
+    password: process.env.DB_PASSWORD || '123456',
     database: process.env.DB_NAME || 'qlsv'
 });
 
